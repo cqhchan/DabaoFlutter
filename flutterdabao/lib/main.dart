0d194115c12934bf9653ede063700349fb9fe1e2
Chris-Phua <chyeegin@hotmail.com>
2018-11-05 15:07:56 +0800

    initialUI

--- flutterdabao/lib/main.dart
+++ flutterdabao/lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:developer';
+import 'package:flutterdabao/app.dart';
 import 'package:observable/observable.dart';
 
 import 'package:flutter/material.dart';
@@ -14,55 +15,55 @@ import 'package:firebase_auth/firebase_auth.dart';
 void main() { 
   
   
-runApp(new MyApp());}
+runApp(new DabaoApp());}
     
-    class MyApp extends StatelessWidget {
-      // This widget is the root of your application.
+//     class MyApp extends StatelessWidget {
+//       // This widget is the root of your application.
 
     
     
-      @override
-      Widget build(BuildContext context) {
+//       @override
+//       Widget build(BuildContext context) {
 
 
-        return new MaterialApp(
-          title: 'Flutter Demo',
-          theme: new ThemeData(
-            // This is the theme of your application.
-            //
-            // Try running your application with "flutter run". You'll see the
-            // application has a blue toolbar. Then, without quitting the app, try
-            // changing the primarySwatch below to Colors.green and then invoke
-            // "hot reload" (press "r" in the console where you ran "flutter run",
-            // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-            // counter didn't reset back to zero; the application is not restarted.
-            primarySwatch: ColorHelper.dabaoOrangeMaterial,
-          ),
-          home: _handleCurrentScreen(),
+//         return new MaterialApp(
+//           title: 'Flutter Demo',
+//           theme: new ThemeData(
+//             // This is the theme of your application.
+//             //
+//             // Try running your application with "flutter run". You'll see the
+//             // application has a blue toolbar. Then, without quitting the app, try
+//             // changing the primarySwatch below to Colors.green and then invoke
+//             // "hot reload" (press "r" in the console where you ran "flutter run",
+//             // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+//             // counter didn't reset back to zero; the application is not restarted.
+//             primarySwatch: ColorHelper.dabaoOrangeMaterial,
+//           ),
+//           home: _handleCurrentScreen(),
     
           
-        );
-      }
+//         );
+//       }
     
-// Handles Authentication State 
-      Widget _handleCurrentScreen() {
-        return new StreamBuilder<FirebaseUser>(
-          stream: FirebaseAuth.instance.onAuthStateChanged,
-          builder: (BuildContext context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-                  return new SplashScreenPage();
-            } else {
-              if (snapshot.hasData) {
-                return new MainTabBarPage(title:"Dabao");
-              }
-                return new LoginPage();
-            }
-          }
-        );
-    }
-    }
+// // Handles Authentication State 
+//       Widget _handleCurrentScreen() {
+//         return new StreamBuilder<FirebaseUser>(
+//           stream: FirebaseAuth.instance.onAuthStateChanged,
+//           builder: (BuildContext context, snapshot) {
+//             if (snapshot.connectionState == ConnectionState.waiting) {
+//                   return new SplashScreenPage();
+//             } else {
+//               if (snapshot.hasData) {
+//                 return new MainTabBarPage(title:"Dabao");
+//               }
+//                 return new LoginPage();
+//             }
+//           }
+//         );
+//     }
+//     }
   
   
-  void temp(String event) {
+//   void temp(String event) {
     
-}
+// }r
