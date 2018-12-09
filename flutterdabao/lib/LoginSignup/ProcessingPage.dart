d0ee7edcf5ad671cf6e7006f4cd366218028e678
LimHengGuang <LimHengGuang@users.noreply.github.com>
2018-12-09 13:17:25 +0800

    Changes to processingpage navigation, but it still isn't navigating correctly

--- flutterdabao/lib/LoginSignup/ProcessingPage.dart
+++ flutterdabao/lib/LoginSignup/ProcessingPage.dart
@@ -23,22 +23,18 @@ class _ProcessingPageState extends State<ProcessingPage> {
 
   @override
   Widget build(BuildContext context) {
-
-
     // There are 3 things that you check to see of it should go to ProfileCreation/Verification or Home
     // return true to go to home/ false to go to ProfileCreation
     return StreamBuilder<bool>(
         stream: Observable.combineLatest3(
             widget.user.email, widget.user.handPhone, widget.user.profileImage,
             (email, phoneNumber, profileImage) {
-
           if (email != null && phoneNumber != null && profileImage != null) {
             return true;
           } else {
             return false;
           }
         }),
-
         builder: (BuildContext context, userSnapshot) {
           if (userSnapshot.connectionState == ConnectionState.waiting ||
               !userSnapshot.hasData) {
@@ -49,25 +45,23 @@ class _ProcessingPageState extends State<ProcessingPage> {
                 return MaterialPageRoute(builder: (context) {
                   return Home();
                 });
-                
               });
             } else {
-
-              if (widget.user.email.value == null){
-                // TODO GO to Verify Email Page
-              }
-
-              if (widget.user.handPhone.value == null){
+              if (widget.user.handPhone.value == null) {
                 // TODO GO to Verify Phone Page
-              }
-
-              if (widget.user.profileImage.value == null){
+                return Navigator(onGenerateRoute: (RouteSettings settings) {
+                  return MaterialPageRoute(builder: (context) {
+                    return VerifyPhoneNumberPage();
+                  });
+                });
+              } else if (widget.user.profileImage.value == null) {
                 // TODO GO to Profile Creation Page
+                return Navigator(onGenerateRoute: (RouteSettings settings) {
+                  return MaterialPageRoute(builder: (context) {
+                    return ProfileCreationPage();
+                  });
+                });
               }
-
-
-              // REMOVE when completed
-              return ProfileCreationPage();
             }
           }
         });
