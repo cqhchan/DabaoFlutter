ad933738fdac04af7e853a5a625d8f607d225dea
Zac Chan <cqhchan@gmail.com>
2019-02-09 19:56:17 +0800

    Fixed IOS google maps no points

--- flutterdabao/lib/ExtraProperties/HavingGoogleMaps.dart
+++ flutterdabao/lib/ExtraProperties/HavingGoogleMaps.dart
@@ -1,37 +1,41 @@
+import 'dart:io';
+
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-
 launchMaps(LatLng location) async {
-  
-  print(location.latitude );
-  print(location.latitude );
+  print(location.latitude);
+  print(location.latitude);
 
   String googleUrl =
-    'comgooglemaps://?center=${location.latitude},${location.longitude}';
+      'comgooglemaps://?center=${location.latitude},${location.longitude}';
   String appleUrl =
-    'https://maps.apple.com/?q=${location.latitude},${location.longitude}';
-  if (await canLaunch("comgooglemaps://")) {
-    print('launching com googleUrl');
-    await launch(googleUrl);
-  } else if (await canLaunch(appleUrl)) {
-    print('launching apple url');
-    await launch(appleUrl);
+      'https://maps.apple.com/?q=${location.latitude},${location.longitude}';
+  if (Platform.isIOS) {
+    if (await canLaunch(appleUrl)) {
+      print('launching apple url');
+      await launch(appleUrl);
+    } else {
+      throw 'Could not launch url';
+    }
   } else {
-    throw 'Could not launch url';
+    if (await canLaunch("comgooglemaps://")) {
+      print('launching com googleUrl');
+      await launch(googleUrl);
+    } else {
+      throw 'Could not launch url';
+    }
   }
 }
 
-
 abstract class HavingGoogleMaps {
-
   GoogleMapController mapController;
   bool isCallBackAdded = true;
   mapCallBack();
 
   Future<void> panToLocation(
-      GoogleMapController controller, LatLng location, double minZoom, [int delayInMilliSecs = 500]) {
-      
+      GoogleMapController controller, LatLng location, double minZoom,
+      [int delayInMilliSecs = 500]) {
     mapController.removeListener(mapCallBack);
     isCallBackAdded = false;
 
@@ -47,16 +51,15 @@ abstract class HavingGoogleMaps {
                 ? controller.cameraPosition.zoom
                 : minZoom),
       ),
-    ).then((complete) {
-      return Future.delayed(Duration(milliseconds: delayInMilliSecs), () => "1");
+    )
+        .then((complete) {
+      return Future.delayed(
+          Duration(milliseconds: delayInMilliSecs), () => "1");
     }).then((s) {
-
-      if (!isCallBackAdded){
-      mapController.addListener(mapCallBack);
-      isCallBackAdded = true;
+      if (!isCallBackAdded) {
+        mapController.addListener(mapCallBack);
+        isCallBackAdded = true;
       }
-
     });
   }
-  
 }
